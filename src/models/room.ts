b5b6660e3e37113dab1f4c0deee6a7e0457ceeaa
Amazia Gur <amaziagur@Amazias-MacBook-Pro.local>
2024-06-14 13:05:07 +0100

    fix: fix room model

--- src/models/room.ts
+++ src/models/room.ts
@@ -2,9 +2,9 @@ export class Room {
     constructor(public id: number, public name: string) {}
 }
 
-// export let rooms: Room[] = [
-//   new Room(1, 'Room A'),
-//   new Room(2, 'Room B'),
-//   new Room(3, 'Room C'),
-//   new Room(4, 'Room D')
-// ];
+export let rooms: Room[] = [
+  new Room(1, 'Room A'),
+  new Room(2, 'Room B'),
+  new Room(3, 'Room C'),
+  new Room(4, 'Room D')
+];
